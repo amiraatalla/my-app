379bbfcca8558853e42e5abd941ed4ea2103900c
amiraatalla <amiraatalla63@gmail.com>
2023-11-04 17:01:48 +0200

    my first react app

---
+++ src/Navbar/Navbar.js
@@ -0,0 +1,11 @@
+import { Component } from "react";
+
+class Navbar extends Component{
+    render(){
+        return<div>
+            <h1>Navbar Component</h1>
+        </div>
+    }
+}
+
+export default Navbar;
